6b56c8e366c87e03bf63d62908008d789572d6ba
Maksym_Woloszynowski <m.woloszynowski.272@studms.ug.edu.pl>
2025-12-29 18:08:13 +0100

    Added new routes

--- backend/src/routes/tweetsRoutes.js
+++ backend/src/routes/tweetsRoutes.js
@@ -1,19 +1,23 @@
 import { Router } from "express";
 import tweetsController from "../controllers/tweetsController.js";
+import authenticateToken from "../middleware/auth.js";
 
 const router = Router();
 
 router.get("/tweets", tweetsController.getAllTweets);
-router.get("/tweets/following", tweetsController.getAllFollowingTweets);
+router.get("/tweets/following", authenticateToken, tweetsController.getAllFollowingTweets);
 router.get("/tweets/:id", tweetsController.getTweet);
 router.get("/tweets/:id/comments", tweetsController.getTweetComments);
 
-router.post("/tweets", tweetsController.createTweet);
-router.post("/tweets/:id/comments", tweetsController.createComment);
+router.post("/tweets/create", authenticateToken, tweetsController.createTweet);
+router.post("/tweets/:id/comments", authenticateToken, tweetsController.createComment);
+router.post("/tweets/:id/like", authenticateToken, tweetsController.likeTweet);
 
-router.post("/tweets/:id/likes", tweetsController.likeTweet);
-router.delete("/tweets/:id/likes", tweetsController.unlikeTweet);
+router.put("/tweets/:id", authenticateToken, tweetsController.editTweet);
+router.put("/tweets/:id/comments/:commentId", authenticateToken, tweetsController.editComment);
 
-router.delete("/tweets/:id", tweetsController.deleteTweet);
+router.delete("/tweets/:id", authenticateToken, tweetsController.deleteTweet);
+router.delete("/tweets/:id/comments/:commentId", authenticateToken, tweetsController.deleteComment);
+router.delete("/tweets/:id/like", authenticateToken, tweetsController.unlikeTweet);
 
 export default router;
