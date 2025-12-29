a60a0f7079d29a71bb5d0b7ad6054ce7fa7bb085
Maksym_Woloszynowski <m.woloszynowski.272@studms.ug.edu.pl>
2025-12-29 18:06:22 +0100

    Added cors and cookies

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,17 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import routes from "./routes/index.js"
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
+
 app.use(express.json());
+app.use(cookieParser())
 app.use(routes)
 
 
