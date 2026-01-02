cc7b3ca154b39723fc723b614a70efc97f395ecf
Maksym_Woloszynowski <m.woloszynowski.272@studms.ug.edu.pl>
2026-01-02 22:08:28 +0100

    Added optimistic followers count

--- frontend/src/components/profile/ProfileStats.jsx
+++ frontend/src/components/profile/ProfileStats.jsx
@@ -1,14 +1,15 @@
 import styles from "./ProfileStats.module.css"
 import { Link } from "react-router-dom";
 
-const ProfileStats = ({ user }) => {
+const ProfileStats = ({ user, followers }) => {
+  
   return (
     <div className={styles.stats}>
       <Link to={`/profile/${user.username}/following`} className={styles.stat}>
         <strong className={styles.thick}>{user.following_count}</strong> Following
       </Link>
       <Link to={`/profile/${user.username}/followers`} className={styles.stat}>
-        <strong className={styles.thick}>{user.followers_count}</strong> Followers
+        <strong className={styles.thick}>{followers}</strong> Followers
       </Link>
     </div>
   );
