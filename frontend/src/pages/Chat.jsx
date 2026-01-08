3e1577998c959cead4073475a704ad0df973d6e6
Maksym_Woloszynowski <m.woloszynowski.272@studms.ug.edu.pl>
2026-01-08 19:39:37 +0100

    Added chat page

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,7 +1,18 @@
+import ContactsList from "../components/chat/ContactsList.jsx";
+import { Outlet } from "react-router-dom";
+import styles from "./Chat.module.css";
+
 const Chat = () => {
   return (
-    <div>Chat</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.contacts}>
+        <ContactsList />
+      </div>
+      <div className={styles.chatContent}>
+        <Outlet />
+      </div>
+    </div>
+  );
+};
 
-export default Chat
+export default Chat;
