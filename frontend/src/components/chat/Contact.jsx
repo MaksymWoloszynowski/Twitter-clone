a3e16b2083829c0c81450467ba304359e05c1a74
Maksym_Woloszynowski <m.woloszynowski.272@studms.ug.edu.pl>
2026-01-09 22:23:42 +0100

    Added contact component

--- frontend/src/components/chat/Contact.jsx
+++ frontend/src/components/chat/Contact.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import styles from "./Contact.module.css";
+import useSocket from "../../hooks/useSocket";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
-const Contact = () => {
+const Contact = ({ contact, isActive, onClick }) => {
+  const socket = useSocket();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleChatId = ({ conversationId }) => {
+      navigate(`/chat/${conversationId}`);
+    };
+
+    socket.on("chat-id", handleChatId);
+
+    return () => {
+      socket.off("chat-id", handleChatId);
+    };
+  }, []);
+
+  const handleClick = () => {
+    onClick();
+    socket.emit("start-chat", { receiverId: contact.user_id });
+  };
+
   return (
-    <div>Contact</div>
-  )
-}
+    <div
+      className={`${styles.contact} ${isActive ? styles.active : ""}`}
+      onClick={handleClick}
+    >
+      <div className={styles.contactAvatar}></div>
+      <div className={styles.contactInfo}>
+        <div className={styles.top}>
+          <p className={styles.contactName}>{contact.username}</p>
+          <p className={styles.createdAt}>
+            {contact.created_at
+              ? new Date(contact.created_at).toLocaleString("pl-PL", {
+                  day: "2-digit",
+                  month: "2-digit",
+                  year: "numeric",
+                  hour: "2-digit",
+                  minute: "2-digit",
+                  hour12: false,
+                })
+              : ""}
+          </p>
+        </div>
+        <p className={styles.lastMessage}>{contact.last_message}</p>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
