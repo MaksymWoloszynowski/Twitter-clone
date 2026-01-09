3203e7b8502f381cdd1b3d70abccf2d338cbe9b4
Maksym_Woloszynowski <m.woloszynowski.272@studms.ug.edu.pl>
2026-01-09 22:21:23 +0100

    Add get contacts functionality and toggle read message

--- backend/src/ws.js
+++ backend/src/ws.js
@@ -49,17 +49,20 @@ export const handleSocketConnection = (io, socket) => {
       [conversationId, userId]
     );
 
-    console.log(member.rowCount);
-    
-
     if (member.rowCount === 0) {
-        socket.emit("error");
-    };
+      socket.emit("error");
+      return;
+    }
 
     socket.join(`conversation:${conversationId}`);
 
+    await pool.query(
+      `UPDATE messages SET read = true WHERE conversation_id = $1 AND receiver_id = $2`,
+      [conversationId, socket.user.id]
+    );
+
     const history = await pool.query(
-      `SELECT id, sender_id, content, created_at
+      `SELECT id, sender_id, content, created_at, read
      FROM messages
      WHERE conversation_id = $1
      ORDER BY created_at ASC`,
@@ -67,6 +70,34 @@ export const handleSocketConnection = (io, socket) => {
     );
 
     socket.emit("chat-history", history.rows);
+  });
+
+  socket.on("get-contacts", async () => {
+    try {
+      const contacts = await pool.query(
+        `SELECT 
+          cm2.user_id,
+          u.username,
+          (SELECT m.content FROM messages m WHERE m.conversation_id = cm1.conversation_id ORDER BY m.created_at DESC LIMIT 1) AS last_message,
+          (SELECT m.read FROM messages m WHERE m.conversation_id = cm1.conversation_id ORDER BY m.created_at DESC LIMIT 1) AS last_read,
+          (SELECT m.created_at FROM messages m WHERE m.conversation_id = cm1.conversation_id ORDER BY m.created_at DESC LIMIT 1) AS created_at,
+          (SELECT COUNT(*) FROM messages m WHERE m.conversation_id = cm1.conversation_id AND m.read = false ) AS unread_count
+        FROM conversation_members cm1
+        JOIN conversation_members cm2 
+            ON cm1.conversation_id = cm2.conversation_id
+        JOIN users u ON u.id = cm2.user_id
+        WHERE cm1.user_id = $1 
+          AND cm2.user_id != $1
+        ORDER BY created_at DESC;`,
+        [socket.user.id]
+      );
+
+      const filteredContacts = contacts.rows.filter(m => m.last_message !== null)
+
+      socket.emit("contacts", filteredContacts);
+    } catch (error) {
+      console.log(error);
+    }
   });
 
   socket.on("send-message", async ({ conversationId, content }) => {
@@ -78,12 +109,20 @@ export const handleSocketConnection = (io, socket) => {
         [conversationId, socket.user.id]
       );
 
-      const receiverId = receiver.rows[0].user_id
+      const receiverId = receiver.rows[0].user_id;
+
+      const sockets = io.sockets.adapter.rooms.get(
+        `conversation:${conversationId}`
+      );
+
+      const read = sockets.size === 2;
+
+      console.log(sockets);
 
       await pool.query(
-        `INSERT INTO messages (conversation_id, sender_id, receiver_id, content)
-         VALUES ($1, $2, $3, $4)`,
-        [conversationId, socket.user.id, receiverId, content]
+        `INSERT INTO messages (conversation_id, sender_id, receiver_id, content, read)
+         VALUES ($1, $2, $3, $4, $5)`,
+        [conversationId, socket.user.id, receiverId, content, read]
       );
 
       io.to(`conversation:${conversationId}`).emit("new-message", {
@@ -91,13 +130,18 @@ export const handleSocketConnection = (io, socket) => {
         receiver_id: receiverId,
         content,
         created_at: new Date(),
+        read,
       });
     } catch (error) {
       console.log(error);
     }
   });
 
-  socket.on("disconnect", () => {
-    console.log("disconnect", userId);
+  socket.on("disconnect", (reason) => {
+    console.log("disconnect", userId, reason);
+  });
+
+  socket.on("leave-conversation", ({ conversationId }) => {
+    socket.leave(`conversation:${conversationId}`);
   });
 };
